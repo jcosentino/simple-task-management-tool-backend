1c7f73cd45d6afb0cf4f27fcd491e439bf9c3a39
jcosentino <johnsc301@gmail.com>
2021-02-25 22:26:23 -0500

    Implemented swagger integration

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require(__dirname + '/swagger/swagger.js');
+const router = express.Router();
 const app = express();
 dotenv.config();
 const port = process.env.PORT ? process.env.PORT : 5000;
@@ -10,6 +13,8 @@ const port = process.env.PORT ? process.env.PORT : 5000;
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 
 if(!process.env.NODE_ENV){ process.env.NODE_ENV = 'development'; }
 console.log(`Starting environment: ${process.env.NODE_ENV}`);
---
+++ src/swagger/swagger.ts
@@ -0,0 +1,172 @@
+require('dotenv').config();
+
+module.exports = {
+    "swagger": "2.0",
+    "info": {
+      "version": "1.0.0",
+      "title": "Simple Task Management Tool",
+      "description": "This application is used to manage your tasks!",
+      "license": {
+        "name": "",
+        "url": ""
+      }
+    },
+    "host": `localhost:${process.env.PORT}`,
+    "basePath": "/",
+    "tags": [
+      {
+        "name": "Task",
+        "description": "API for CRUD functionality for Tasks management"
+      }
+    ],
+    "schemes": [
+      "http"
+    ],
+    "consumes": [
+      "application/json"
+    ],
+    "produces": [
+      "application/json"
+    ],
+    "paths": {
+      "/tasks": {
+        "get": {
+          "tags": [
+            "Task"
+          ],
+          "description": "Retrieve all tasks",
+          "parameters": [],
+          "produces": [
+            "application/json"
+          ],
+          "responses": {
+            "200": {
+              "description": "Retrieves all tasks, empty array is none are present",
+              "schema": {
+                "$ref": "#/definitions/Tasks"
+              }
+            }
+          }
+        }
+      },
+      "/task": {
+        "post": {
+          "tags": [
+            "Task"
+          ],
+          "description": "Create new user in system",
+          "parameters": [
+            {
+              "name": "task",
+              "in": "body",
+              "description": "Create a task",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          ],
+          "produces": [
+            "application/json"
+          ],
+          "responses": {
+            "200": {
+              "description": "New task gets created",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          }
+        }
+      },
+      "/task/{id}": {
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "description": "ID primary key of task",
+            "type": "string"
+          }
+        ],
+        "get": {
+          "tags": [
+            "task"
+          ],
+          "summary": "Retrieve task with given ID",
+          "responses": {
+            "200": {
+              "description": "Task is retrieved",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          }
+        },
+        "delete": {
+          "summary": "Delete task with given ID",
+          "tags": [
+            "Task"
+          ],
+          "responses": {
+            "200": {
+              "description": "Task is deleted",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          }
+        },
+        "put": {
+          "summary": "Update task with given ID",
+          "tags": [
+            "Task"
+          ],
+          "parameters": [
+            {
+              "name": "task",
+              "in": "body",
+              "description": "Updated task entry with new values",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          ],
+          "responses": {
+            "200": {
+              "description": "Task has been updated!",
+              "schema": {
+                "$ref": "#/definitions/Task"
+              }
+            }
+          }
+        }
+      }
+    },
+    "definitions": {
+      "Task": {
+        "required": [
+          "id",
+          "name"
+        ],
+        "properties": {
+          "id": {
+            "type": "integer",
+            "uniqueItems": true
+          },
+          "name": {
+            "type": "string"
+          },
+          "description": {
+            "type": "string"
+          },
+          "dueDate": {
+            "type": "string"
+          },
+          "status": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+  
